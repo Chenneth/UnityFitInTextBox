2832d7b3da0b7b16e40f3cd0f3e6d7a72c7d3e5d
Kenneth Chen <74205780+Chenneth@users.noreply.github.com>
2022-07-14 12:47:40 -0500

    Addition of HelperClass
    Corresponding functions from 'TestClass' in TextFit.cs moved to 'HelperClass'

---
+++ HelperClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class TestClass
+{
+    public static bool TryFindIndices<T>(this IEnumerable<T> items, Func<T, bool> predicate, out IEnumerable<int> indices) 
+    {
+        int i = 0;
+        List<int> indicesList = new List<int>(1);
+        foreach (var item in items) 
+        {
+            if (predicate(item))
+            {
+                indicesList.Add(i);
+            }
+
+            i++;
+        }
+
+        indices = indicesList.AsEnumerable();
+        return indicesList.Count > 0;
+    } //modified from sta ckover flow.c om/questions/14476162/14476244#14476244
+}
--- TextFit.cs
+++ TextFit.cs
@@ -217,24 +217,3 @@ public class TextFit : MonoBehaviour
         return false;
     }*/
 }
-
-public static class TestClass
-{
-    public static bool TryFindIndices<T>(this IEnumerable<T> items, Func<T, bool> predicate, out IEnumerable<int> indices) 
-    {
-        int i = 0;
-        List<int> indicesList = new List<int>(1);
-        foreach (var item in items) 
-        {
-            if (predicate(item))
-            {
-                indicesList.Add(i);
-            }
-
-            i++;
-        }
-
-        indices = indicesList.AsEnumerable();
-        return indicesList.Count > 0;
-    } //modified from sta ckover flow.c om/questions/14476162/14476244#14476244
-}
